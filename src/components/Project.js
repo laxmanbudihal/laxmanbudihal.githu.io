fba0a886ca76ca05249d57ead42183ee1b243b72
Laxman Budihal <laxmanbudihal0321@gmail.com>
2020-12-29 22:15:35 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -35,14 +35,14 @@ function Project()  {
                         <Card className="mt-2 mb-2">
     <Card.Img variant="top" src="/a.png" />
     <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>To-Do app</Card.Title>
         <Card.Text>
         Some quick example text to build on the card title and make up the bulk of
         the card's content.
         </Card.Text>
         <Button
          onClick={()=>{
-             window.open("https://codesandbox.io/s/react-snippet-forked-6pmey?file=/src/styles.css","_blank")
+             window.open("https://laxmanbudihal-todo.netlify.app/")
          }
          }
           variant="primary">Go somewhere</Button>
@@ -56,8 +56,7 @@ function Project()  {
     <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
+        To Do App it will help you to Save a note of Work And After the completion of Work if you click on the work Message box then it Show you that the work is Completed
         </Card.Text>
         <Button
          onClick={()=>{
